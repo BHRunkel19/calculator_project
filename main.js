229716412d8b68d70d731db26e9763a611a0cfd4
BubbaBean <brandon.runkel@gmail.com>
2017-07-17 16:36:52 -0400

    functionality is working for the most part

--- main.js
+++ main.js
@@ -8,11 +8,27 @@ console.log("I'm ready for some calculations!");
 // header.style.textAlign="center";
 // header.style.fontSize="xx-large";
 
-var input = document.querySelector("#display");
-  console.log(input);
+var display = document.querySelector("#display");
   // selects display input field and sets it to variable input
-var keys = document.querySelectorAll('.num');
-  console.log(keys);
-  //creates array of button elements in variable keys
+var buttons = document.querySelectorAll('.num');
+console.log(buttons);
+  let values = [];
+  for (i=0; i<buttons.length; i++){
+    // values.push(buttons[i].innerHTML);
+    // console.log(values);
+    buttons[i].addEventListener('click', function(){
+      display.innerHTML += event.target.value;
+      values.push(event.target.value);
+    });
+  }
 
-//Need to assign function to event listener that displays button value in display field
+  var equals = document.querySelector('#equals');
+  equals.addEventListener('click', function(){
+    console.log(eval(values.join("")));
+    display.innerHTML=(eval(values.join("")));
+  })
+
+  var clear = document.querySelector('#clear');
+  clear.addEventListener('click', function(){
+
+  })
