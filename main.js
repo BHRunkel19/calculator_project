8491a0eac36ad6092cbc28f779cf01f7afd9193a
BubbaBean <brandon.runkel@gmail.com>
2017-07-16 22:10:05 -0400

    layout design and button functionality working

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("I'm ready");
+
+//Define range of numbers
+// let numbers = [0,1,2,3,4,5,6,7,8,9]
+//
+// //Select from range of numbers
+// function calculator(){
+//
+// let select =[];
+//
+// for (i=0; i <)
+
+let body = document.querySelector("body");
+let header = document.createElement("h1");
+let headerTitle = document.createTextNode("Javascript Calculator");
+header.appendChild(headerTitle);
+body.appendChild(header);
+header.style.textAlign="center";
+header.style.fontSize="xx-large";
+
+
+function displayVal(){
+var input = document.querySelector("#display");
+  // console.log(input);
+var keys = document.querySelectorAll("input");
+  // console.log(keys);
+let value = keys.innerHTML;
+
+}
