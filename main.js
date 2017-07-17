a53140d30257e0f2758556075fccc6e64ecffc0e
BubbaBean <brandon.runkel@gmail.com>
2017-07-17 11:13:46 -0400

    working on formatting and functionality

--- main.js
+++ main.js
@@ -1,29 +1,18 @@
-console.log("I'm ready");
+console.log("I'm ready for some calculations!");
 
-//Define range of numbers
-// let numbers = [0,1,2,3,4,5,6,7,8,9]
-//
-// //Select from range of numbers
-// function calculator(){
-//
-// let select =[];
-//
-// for (i=0; i <)
+// let body = document.querySelector("body");
+// let header = document.createElement("h1");
+// let headerTitle = document.createTextNode("Javascript Calculator");
+// header.appendChild(headerTitle);
+// body.appendChild(header);
+// header.style.textAlign="center";
+// header.style.fontSize="xx-large";
 
-let body = document.querySelector("body");
-let header = document.createElement("h1");
-let headerTitle = document.createTextNode("Javascript Calculator");
-header.appendChild(headerTitle);
-body.appendChild(header);
-header.style.textAlign="center";
-header.style.fontSize="xx-large";
+var input = document.querySelector("#display");
+  console.log(input);
+  // selects display input field and sets it to variable input
+var keys = document.querySelectorAll('.num');
+  console.log(keys);
+  //creates array of button elements in variable keys
 
-
-// function displayVal(){
-// var input = document.querySelector("#display");
-//   // console.log(input);
-// var keys = document.querySelectorAll("input");
-//   // console.log(keys);
-// let value = keys.innerHTML;
-//
-// }
+//Need to assign function to event listener that displays button value in display field
